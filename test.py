318cc20f8452a5a52079c0199e4a1f0c4119aa86
Anish Rao <ansrao@ucsc.edu>
2023-04-17 16:04:36 -0400

    added household recode

--- test.py
+++ test.py
@@ -1,19 +1,44 @@
 import pandas as pd
 
-# Load the dataset
-data = pd.read_stata('https://github.com/ansrao-git/DHS-testing/raw/main/ETKR70FL.DTA')
+data = pd.read_csv('childrecode.csv')
 
-# Create binary target variable for stunting
-data['stunting'] = data['HAZ_score'] < -2
-data['stunting'] = data['stunting'].astype(int)
+weight_age_sd = 'hw8'
+height_age_sd = 'hw5'
+weight_height_sd = 'hw11'
 
-# Create binary target variable for wasting
-data['wasting'] = data['WHZ_score'] < -2
-data['wasting'] = data['wasting'].astype(int)
+#new WHO values
+weight_age_sd_new = 'hw71'
+height_age_sd_new = 'hw70'
+weight_height_sd_new = 'hw72'
 
-# Create binary target variable for underweight
-data['underweight'] = data['WAZ_score'] < -2
-data['underweight'] = data['underweight'].astype(int)
+# create stunting column
+data['stunting'] = data[height_age_sd_new] < -200
+#data['severe_stunting'] = data[height_age_sd_new] < -300
 
-# Print the first five rows of the dataset with the new target variables
-print(data[['stunting', 'wasting', 'underweight']].head())
+# create wasting column
+data['wasting'] = data[weight_height_sd_new] < -200
+#data['severe_wasting'] = data[weight_height_sd_new] < -300
+
+# create underweight column
+data['underweight'] = data[weight_age_sd_new] < -200
+#data['severe_underweight'] = data[weight_age_sd_new] < -300
+
+data = data[data['hw2'] <= 9997] # Removing missing
+data = data[data['hw3'] <= 9997] # Removing missing
+
+#Divide by 10 to remove implied decimal place
+data['hw3'] = data['hw3'] / 10
+data['hw2'] = data['hw2'] / 10
+
+df_new = pd.DataFrame({
+    'case_id': data['caseid'],
+    'weight': data['hw2'],
+    'age': data['b8'],
+    'height': data['hw3'],
+    'stunting': data['stunting'],
+    'wasting': data['wasting'] ,
+    'underweight': data['underweight']
+})
+
+
+print(df_new.head())
