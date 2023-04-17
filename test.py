1f3c700aae0604b66abd8e14ed19919c7690251e
Anish Rao <ansrao@ucsc.edu>
2023-04-17 17:12:08 -0400

    d

--- test.py
+++ test.py
@@ -1,44 +1,17 @@
 import pandas as pd
 
-data = pd.read_csv('childrecode.csv')
+child = pd.read_csv('DHS-testing\childrecode.csv')
+house = pd.read_csv('DHS-testing\householdrecode.csv')
 
-weight_age_sd = 'hw8'
-height_age_sd = 'hw5'
-weight_height_sd = 'hw11'
+# create set of column names for each dataframe
+set1 = set(child.columns)
+set2 = set(house.columns)
 
-#new WHO values
-weight_age_sd_new = 'hw71'
-height_age_sd_new = 'hw70'
-weight_height_sd_new = 'hw72'
+# find unique columns in each set
+unique_cols_child = set1 - set2
+unique_cols_household = set2 - set1
 
-# create stunting column
-data['stunting'] = data[height_age_sd_new] < -200
-#data['severe_stunting'] = data[height_age_sd_new] < -300
-
-# create wasting column
-data['wasting'] = data[weight_height_sd_new] < -200
-#data['severe_wasting'] = data[weight_height_sd_new] < -300
-
-# create underweight column
-data['underweight'] = data[weight_age_sd_new] < -200
-#data['severe_underweight'] = data[weight_age_sd_new] < -300
-
-data = data[data['hw2'] <= 9997] # Removing missing
-data = data[data['hw3'] <= 9997] # Removing missing
-
-#Divide by 10 to remove implied decimal place
-data['hw3'] = data['hw3'] / 10
-data['hw2'] = data['hw2'] / 10
-
-df_new = pd.DataFrame({
-    'case_id': data['caseid'],
-    'weight': data['hw2'],
-    'age': data['b8'],
-    'height': data['hw3'],
-    'stunting': data['stunting'],
-    'wasting': data['wasting'] ,
-    'underweight': data['underweight']
-})
-
-
-print(df_new.head())
+# print number of unique columns in each set
+print("Number of columns in child dataframe:", len(set1))
+print("Number of unique columns in child dataframe:", len(unique_cols_child))
+print("Number of unique columns in household dataframe:", len(unique_cols_household))
